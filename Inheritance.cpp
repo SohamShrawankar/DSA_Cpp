74710766380dfc4ca080d0abce9e41d21357f52e
Soham_Shrawankar@ <soham.shrawankar.btech2022@sitnagpur.siu.edu.in>
2025-10-19 06:41:06 +0530

    Add vehicle and car classes for single inheritance
    Implement single inheritance with vehicle and car classes.

---
+++ Inheritance.cpp
@@ -0,0 +1,28 @@
+//SINGLE INHERITANCE 
+
+#include<bits/stdc++.h>
+using namespace std ;
+
+class vehicle{
+     public:
+    vehicle(){
+            cout<<"Hi my name is " ;
+    }
+};
+
+class car : public vehicle{
+    public:
+    car(){
+           cout<<"soham shrawankar " ;
+    }
+};
+
+int main(){
+    car obj ;
+
+    return 0;
+}
+
+____________________________________
+
+
