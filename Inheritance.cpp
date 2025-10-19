1807d9dbded7f56e048fa60a802277a5de7dfd51
Soham_Shrawankar@ <soham.shrawankar.btech2022@sitnagpur.siu.edu.in>
2025-10-19 07:30:30 +0530

    Update Inheritance.cpp

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -57,4 +57,36 @@ int main() {
 
 
 _________________________________
+// MULTILEVEL INHERITANCE 
 
+#include<bits/stdc++.h>
+using namespace std ;
+
+class class1{
+    public:
+    class1 (){
+        cout<<"Hi my name is " ;
+    }
+};
+class class2 : public class1{
+    public:
+    class2 (){
+        cout<<"Soham Shrawankar " ;
+    }
+};
+class class3 : public class2{
+    public:
+    class3 (){
+        cout<<"A student of SIT nagpur. " ;
+    }
+};
+
+
+int main(){
+  
+    class3 obj;
+
+    return 0 ;
+}
+
+________________________________
