46ca0055fad6323204102a0e489964ed1f51668e
Soham_Shrawankar@ <soham.shrawankar.btech2022@sitnagpur.siu.edu.in>
2025-10-19 06:59:46 +0530

    Update Inheritance.cpp

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -26,35 +26,35 @@ int main(){
 ____________________________________
 // MULTIPLE INHERITANCE 
 
-#include<bits/stdc++.h>
-using namespace std ;
+#include <bits/stdc++.h>
+using namespace std;
 
-class vehicle{
-     public:
-    vehicle(){
-            cout<<"Hi my name is " ;
+class class1 {
+public:
+    class1() {
+        cout << "Hi my name is ";
     }
 };
 
-class car :virtual public vehicle{
-    public:
-    car(){
-           cout<<"soham shrawankar " ;
+class class2 : virtual public class1 {
+public:
+    class2() {
+        cout << "Soham Shrawankar ";
     }
 };
 
-class cycle :virtual public vehicle, public car{
-    public:
-    cycle(){
-           cout<<"a student from SIT nagpur. " ;
+class class3 : virtual public class1, public class2 {
+public:
+    class3() {
+        cout << "a student from SIT Nagpur.";
     }
 };
 
-int main(){
-    cycle obj ;
-
+int main() {
+    class3 obj;
     return 0;
 }
+
 
 _________________________________
 
